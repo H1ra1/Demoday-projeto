27b06fcbb3a04d8974733a7226ced75e1d7a5751
Hira <hlira96@gmail.com>
2019-08-17 20:07:30 -0300

    criar model Receita

--- app/models.py
+++ app/models.py
@@ -69,3 +69,79 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name = 'Usuário'
         verbose_name_plural = 'Usuários'
 
+
+class Receita(models.Model):
+
+    PRATOS = (
+        ('Q', 'Quente'),
+        ('F', 'Frio'),
+    )
+
+    TIPOS_PRATO = (
+        ('S', 'Salgado'),
+        ('D', 'Doce'),
+        ('Vga', 'Vegano'),
+        ('Vge', 'Vegetariano'),
+    )
+
+    usuario = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+    )
+
+    id_receita = models.AutoField(
+        primary_key= True,
+        auto_created= True,
+        verbose_name = 'ID receita',
+        )
+
+    nome_receita = models.CharField(
+        max_length = 255,
+        verbose_name = 'Nome da receita',
+    )
+
+    receita = models.TextField(
+        verbose_name = 'Receita',
+    )
+
+    descricao_receita = models.CharField(
+        max_length = 255,
+        verbose_name = 'Descrição da receita',
+    )
+
+    ingredientes = models.TextField(
+        verbose_name = 'Ingredientes',
+    )
+
+    foto_receita = models.ImageField(
+        upload_to = 'receitas_fotos/',
+        verbose_name = 'Foto da receita',
+    )
+
+    prato = models.CharField(
+        max_length = 255,
+        choices = PRATOS,
+        verbose_name = 'Prato',
+    )
+
+    tipo_prato = models.CharField(
+        max_length = 255,
+        choices = TIPOS_PRATO,
+        verbose_name = 'Tipo do prato',
+    )
+
+    aprovado = models.BooleanField(
+        default = False,
+        verbose_name = 'Aprovar receita',
+    )
+
+    data_criacao = models.DateField(
+        auto_now_add = True,
+    )
+
+    def __str__(self):
+        return self.nome_receita
+
+
+
+
